d9a9e2bf3af04f7f5f1f2cb957243b8c45c4f9ce
Thanu06 <70212045+Thanu06@users.noreply.github.com>
2022-06-07 20:01:22 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -55,7 +55,7 @@ function draw() {
 }
 
 function keyPressed(){
-	if (keyCode === UP_ARROW) {
+	if (keyWentDown === UP_ARROW) {
 
 		Matter.Body.applyForce(paper.body,paper.body.position,{x:85,y:-85});
 	}
